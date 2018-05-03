e5f175295bfa2026d3b5561e77344f537ecd4ec5
fcu-d0143916 <saturnflion@gmail.com>
2018-05-03 17:13:18 +0800

    建立下拉式選單  增加選項

--- app/src/main/java/tw/edu/fcu/lukeway/thesunrise/MainActivity.java
+++ app/src/main/java/tw/edu/fcu/lukeway/thesunrise/MainActivity.java
@@ -2,6 +2,8 @@ package tw.edu.fcu.lukeway.thesunrise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner_place);
+        ArrayAdapter<CharSequence> lunchList = ArrayAdapter.createFromResource(MainActivity.this,
+                R.array.spinner_place,
+                android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(lunchList);
     }
 }
