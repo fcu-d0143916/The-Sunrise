6be919f5576fa3d75df9dfe689524f3b56246e58
fcu-d0143916 <D0143916@fcu.edu.tw>
2018-05-17 14:08:49 +0800

    主頁面更改。備份

--- app/src/main/java/tw/edu/fcu/lukeway/thesunrise/MainActivity.java
+++ app/src/main/java/tw/edu/fcu/lukeway/thesunrise/MainActivity.java
@@ -1,11 +1,51 @@
 package tw.edu.fcu.lukeway.thesunrise;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ListView 要顯示的內容　改到全域變數
+    public String[] str = {"新北市","台北市","台中市","台南市","高雄市"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listview = (ListView) findViewById(R.id.listview);
+
+        //android.R.layout.simple_list_item_1 為內建樣式，還有其他樣式可自行研究
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                str);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(onClickListView);       //指定事件 Method
+
+    }
+
+    /***
+     * 點擊ListView事件Method
+     */
+    private AdapterView.OnItemClickListener onClickListView = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Toast 快顯功能 第三個參數 Toast.LENGTH_SHORT 2秒  LENGTH_LONG 5秒
+            Toast.makeText(MainActivity.this,"點選第 "+(position +1) +" 個 \n內容："+str[position], Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    /*ArrayAdapter <String> adapter;
+    String[] name = {"A地點", "B地點", "C地點", "D地點", "E地點"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
                 R.array.spinner_place,
                 R.layout.myspinner);
         spinner.setAdapter(lunchList);
-    }
+        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,name);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }
+        });
+    }*/
 }
+
