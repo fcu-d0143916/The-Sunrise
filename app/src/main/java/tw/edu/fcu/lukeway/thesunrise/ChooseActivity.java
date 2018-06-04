476db2c82221e5caa2acca36a9e61a028db59856
fcu-d0143916 <D0143916@fcu.edu.tw>
2018-06-04 15:39:54 +0800

    補日出日沒圖示，各頁面切換

--- app/src/main/java/tw/edu/fcu/lukeway/thesunrise/ChooseActivity.java
+++ app/src/main/java/tw/edu/fcu/lukeway/thesunrise/ChooseActivity.java
@@ -1,7 +1,11 @@
 package tw.edu.fcu.lukeway.thesunrise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChooseActivity extends AppCompatActivity {
@@ -10,10 +14,60 @@ public class ChooseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        ///////////////////////////////////////////////////////////////天氣、影像、地圖三個按鈕
+        Button btn_weather = (Button) findViewById(R.id.btm_weather);
+        Button btm_liveimage = (Button) findViewById(R.id.btm_liveimage);
+        Button btm_map = (Button) findViewById(R.id.btm_map);
+
+        btn_weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(ChooseActivity.this, WeatherActivity.class);
+                //Bundle bundle = new Bundle();
+                //bundle.putInt("Choose_num", position +1);
+                //intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        btm_liveimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(ChooseActivity.this, LiveImageActivity.class);
+                //Bundle bundle = new Bundle();
+                //bundle.putInt("Choose_num", position +1);
+                //intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        btm_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(ChooseActivity.this, MapActivity.class);
+                //Bundle bundle = new Bundle();
+                //bundle.putInt("Choose_num", position +1);
+                //intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        ///////////////////////////////////////////////////////////////
+        TextView uptime = (TextView)findViewById(R.id.up_time);// 取得 日出資料
+        TextView downtime = (TextView)findViewById(R.id.down_time);// 取得 日沒資料
 
         Bundle bundle = this.getIntent().getExtras();
         int choose_num = bundle.getInt("Choose_num");
+
+
+        if(choose_num == 1) {
+            uptime.setText("00:00");
+            downtime.setText("11:11");
+        }
+
         //測試值是否有傳過來
-        Toast.makeText(ChooseActivity.this,"點選第 "+(choose_num) +" 個 \n內容："+choose_num, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(ChooseActivity.this,"點選第 "+(choose_num) +" 個 \n內容："+choose_num, Toast.LENGTH_SHORT).show();
     }
 }
