41ccb28a5f1b0f833f06a4a850378e1314d62545
fcu-d0143916 <D0143916@fcu.edu.tw>
2018-06-20 00:57:05 +0800

    新增中央氣象局網頁頁面連結

--- app/src/main/java/tw/edu/fcu/lukeway/thesunrise/WeatherActivity.java
+++ app/src/main/java/tw/edu/fcu/lukeway/thesunrise/WeatherActivity.java
@@ -1,11 +1,14 @@
 package tw.edu.fcu.lukeway.thesunrise;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -15,6 +18,8 @@ import java.net.URL;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    private Button button;
+
     private ImageView img;
 
     @SuppressLint("StaticFieldLeak")
@@ -23,10 +28,23 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        img = (ImageView) findViewById(R.id.image_weather);
-
         Bundle bundle = this.getIntent().getExtras();
         final int choose_num = bundle.getInt("Choose_num");
+
+        Button nextPageBtn = (Button)findViewById(R.id.button_web);
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(WeatherActivity.this, WebWeather.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("Choose_num", choose_num);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        img = (ImageView) findViewById(R.id.image_weather);
 
         if (choose_num == 1) {
             //建立一個AsyncTask執行緒進行圖片讀取動作，並帶入圖片連結網址路徑
---
+++ app/src/main/java/tw/edu/fcu/lukeway/thesunrise/WebWeather.java
@@ -0,0 +1,39 @@
+package tw.edu.fcu.lukeway.thesunrise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WebWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_weather);
+
+        Bundle bundle = this.getIntent().getExtras();
+        final int choose_num = bundle.getInt("Choose_num");
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //setContentView(webview);
+        webview.setWebViewClient(new WebViewClient());
+        //webview.loadUrl("https://google.com");
+
+        if (choose_num == 1) {//合歡山 小風口停車場
+            webview.loadUrl("https://www.cwb.gov.tw/V7/forecast/entertainment/other/F002.htm");
+            //webview.loadUrl("http://117.56.55.25/T14A-36K+560");
+        }
+        if (choose_num == 2) {//合歡山 武嶺亭
+            //webview.loadUrl("file:///android_asset/webview_2.html");
+            Toast.makeText(WebWeather.this,"此資料無中央氣象局網站頁面。", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+}
